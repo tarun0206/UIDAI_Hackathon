00cafcf0f26fab398632d10bf422205b971a2f92
tarun0206 <tarun.singh.0206@gmail.com>
2021-10-31 14:25:16 +0530

    removed previous api

--- modules/uidai/routes.py
+++ modules/uidai/routes.py
@@ -12,30 +12,6 @@ import json
 uidai_module = Blueprint('uidai', __name__)
 CORS(app)
 
-@app.route('/new', methods=['GET'])
-@cross_origin()
-def neww():
-    csv_data = pd.read_csv('new_data.csv')
-    try :
-        data = []
-        resp = {}
-        csv_data_org = csv_data.loc[: , ['building','street','landmark','locality','vtc','district','state']]
-        csv_data_list = csv_data_org.values.tolist()
-        for row in csv_data_list:
-            vtc = str(row[4])
-            district = str(row[5])
-            if str(vtc) == str(district):
-                n_vtc = None
-            else:
-                n_vtc = vtc         
-            record_dict = {'building' : row[0], 'street' : row[1], 'landmark' : row[2], 'locality' : row[3], 'vtc' : n_vtc, 'district' : row[5], 'state' : row[6]}
-            data.append(record_dict)
-        resp['data'] = data
-        return jsonify(resp)
-    except :
-        pass        
-
-
 @app.route('/newapi', methods=['GET'])
 @cross_origin()
 def nnewapi():
